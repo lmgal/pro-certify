294f1383768ead6344359b09dc9190c95499fb4c
Louie Gallos <lmgallos@up.edu.ph>
2023-06-18 19:29:43 +0800

    Fixed typescript error on wallet context

--- context/wallet.tsx
+++ context/wallet.tsx
@@ -1,6 +1,6 @@
 import { useState, createContext, useContext } from "react"
-// @ts-expect-error
 import { BeaconWallet } from "@taquito/beacon-wallet"
+import { NetworkType } from "@airgap/beacon-sdk"
 
 type WalletProviderProps = {
     children: React.ReactNode
@@ -8,12 +8,13 @@ type WalletProviderProps = {
 
 const WalletContext = createContext<BeaconWallet | null>(null)
 
+// @ts-expect-error
 export const useWallet = () => useContext<BeaconWallet>(WalletContext)
 
 export const WalletProvider = (props: WalletProviderProps) => {
     const [wallet] = useState(new BeaconWallet({
         name: 'ProCertify',
-        preferredNetwork: 'ghostnet'
+        preferredNetwork: process.env.NEXT_PUBLIC_TZ_NETWORK as NetworkType
     }))
 
     return (
