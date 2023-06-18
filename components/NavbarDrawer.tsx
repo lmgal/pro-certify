648c3850868e83b57f5c6713fa8298828a30c13d
Louie Gallos <lmgallos@up.edu.ph>
2023-06-18 19:26:11 +0800

    Added navigation bar + drawer for authenticated pages

---
+++ components/NavbarDrawer.tsx
@@ -0,0 +1,211 @@
+import { useState, useEffect } from 'react'
+import { NetworkType } from '@airgap/beacon-sdk'
+
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import { styled } from '@mui/material/styles'
+import AddIcon from '@mui/icons-material/Add'
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Badge from '@mui/material/Badge'
+import MuiDrawer from '@mui/material/Drawer'
+import Divider from '@mui/material/Divider'
+import List from '@mui/material/List'
+import Toolbar from '@mui/material/Toolbar'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Tooltip from '@mui/material/Tooltip'
+
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import MenuIcon from '@mui/icons-material/Menu'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import HomeIcon from '@mui/icons-material/Home'
+
+import { useWallet } from '@/context/wallet'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useRouter } from 'next/router'
+
+const drawerWidth: number = 240
+
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean
+}
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}))
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        '& .MuiDrawer-paper': {
+            position: 'relative',
+            whiteSpace: 'nowrap',
+            width: drawerWidth,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            boxSizing: 'border-box',
+            ...(!open && {
+                overflowX: 'hidden',
+                transition: theme.transitions.create('width', {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.leavingScreen,
+                }),
+                width: theme.spacing(7),
+                [theme.breakpoints.up('sm')]: {
+                    width: theme.spacing(9),
+                },
+            }),
+        },
+    }),
+)
+
+export default function NavbarDrawer() {
+    const supabase = useSupabaseClient()
+    const wallet = useWallet()
+    const router = useRouter()
+
+    const [address, setAddress] = useState('')
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const [open, setOpen] = useState(true)
+
+    const connectWallet = async () => {
+        await wallet.requestPermissions({
+            network: {
+                type: process.env.NEXT_PUBLIC_TZ_NETWORK as NetworkType
+            }
+        })
+        const address = await wallet.getPKH()
+        setAddress(address)
+    }
+
+    const signOut = async () => {
+        await Promise.all([supabase.auth.signOut(), wallet.clearActiveAccount()])
+        router.push('/sign-in')
+    }
+
+    const toggleDrawer = () => {
+        setOpen(!open)
+    }
+
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
+    useEffect(() => {
+        (async () => {
+            const account = await wallet.client.getActiveAccount()
+            setAddress(account ? account.address : '')
+        })()
+    }, [])
+
+    return (
+        <>
+            <AppBar position="absolute" open={open}>
+                <Toolbar
+                    sx={{
+                        pr: '24px', // keep right padding when drawer closed
+                    }}
+                >
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={toggleDrawer}
+                        sx={{
+                            marginRight: '36px',
+                            ...(open && { display: 'none' }),
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        component="h1"
+                        variant="h6"
+                        color="inherit"
+                        noWrap
+                        sx={{ flexGrow: 1 }}
+                    >
+                        Dashboard
+                    </Typography>
+                    <IconButton color="inherit" onClick={handleMenu}>
+                        <Badge badgeContent={4} color="secondary">
+                            <Tooltip title={address}>
+                                <AccountCircleIcon />
+                            </Tooltip>
+                        </Badge>
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        {!address && <MenuItem onClick={connectWallet}>Connect wallet</MenuItem>}
+                        <MenuItem onClick={signOut}>Sign out</MenuItem>
+                    </Menu>
+                </Toolbar>
+            </AppBar>
+            <Drawer variant="permanent" open={open}>
+                <Toolbar
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'flex-end',
+                        px: [1],
+                    }}
+                >
+                    <IconButton onClick={toggleDrawer}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </Toolbar>
+                <Divider />
+                <List component="nav">
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Dashboard" />
+                    </ListItemButton>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <AddIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Add template" />
+                    </ListItemButton>
+                    <Divider sx={{ my: 1 }} />
+                </List>
+            </Drawer>
+        </>
+    )
+}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,30 +1,13 @@
 import { GetServerSideProps, GetServerSidePropsContext } from 'next'
-import { useWallet } from '@/context/wallet'
 import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
 
 import Box from '@mui/material/Box'
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
-import Badge from '@mui/material/Badge'
-import MenuIcon from '@mui/icons-material/Menu'
-import NotificationsIcon from '@mui/icons-material/Notifications'
-import MuiDrawer from '@mui/material/Drawer'
-import Divider from '@mui/material/Divider'
-import List from '@mui/material/List'
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemText from '@mui/material/ListItemText'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import { styled } from '@mui/material/styles'
-import { useState } from 'react'
-import AddIcon from '@mui/icons-material/Add'
 
-
+import NavbarDrawer from '@/components/NavbarDrawer'
 import Copyright from '@/components/Copyright'
 import Templates from '@/components/Templates'
 
@@ -38,124 +21,12 @@ type DashboardProps = {
     }[] | null
 }
 
-const drawerWidth: number = 240
-
-interface AppBarProps extends MuiAppBarProps {
-    open?: boolean
-}
-
-const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }),
-}))
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-        '& .MuiDrawer-paper': {
-            position: 'relative',
-            whiteSpace: 'nowrap',
-            width: drawerWidth,
-            transition: theme.transitions.create('width', {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-            boxSizing: 'border-box',
-            ...(!open && {
-                overflowX: 'hidden',
-                transition: theme.transitions.create('width', {
-                    easing: theme.transitions.easing.sharp,
-                    duration: theme.transitions.duration.leavingScreen,
-                }),
-                width: theme.spacing(7),
-                [theme.breakpoints.up('sm')]: {
-                    width: theme.spacing(9),
-                },
-            }),
-        },
-    }),
-)
-
-
 export default function Dashboard(props: DashboardProps) {
     const { templates } = props
-    const [open, setOpen] = useState(true)
-    const toggleDrawer = () => {
-        setOpen(!open)
-    }
 
     return (
         <Box sx={{ display: 'flex' }}>
-            <AppBar position="absolute" open={open}>
-                <Toolbar
-                    sx={{
-                        pr: '24px', // keep right padding when drawer closed
-                    }}
-                >
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={toggleDrawer}
-                        sx={{
-                            marginRight: '36px',
-                            ...(open && { display: 'none' }),
-                        }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography
-                        component="h1"
-                        variant="h6"
-                        color="inherit"
-                        noWrap
-                        sx={{ flexGrow: 1 }}
-                    >
-                        Dashboard
-                    </Typography>
-                    <IconButton color="inherit">
-                        <Badge badgeContent={4} color="secondary">
-                            <NotificationsIcon />
-                        </Badge>
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent" open={open}>
-                <Toolbar
-                    sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'flex-end',
-                        px: [1],
-                    }}
-                >
-                    <IconButton onClick={toggleDrawer}>
-                        <ChevronLeftIcon />
-                    </IconButton>
-                </Toolbar>
-                <Divider />
-                <List component="nav">
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <AddIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Add template" />
-                    </ListItemButton>
-                    <Divider sx={{ my: 1 }} />
-                </List>
-            </Drawer>
+            <NavbarDrawer />
             <Box
                 component="main"
                 sx={{
